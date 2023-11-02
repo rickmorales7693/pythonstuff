cf305d4e774c5313df92b5e511fd8f77b92afa1f
Rick Morales <rick.morales7693@gmail.com>
2023-11-02 11:32:15 -0500

    worked on basics of variables

---
+++ application.py
@@ -0,0 +1,24 @@
+print("Hellow World")
+print('this is the next statement')
+
+number = 8852
+print(number)
+
+weight = 22
+
+print(weight)
+
+answer = number + weight
+
+print(answer)
+
+first_name = "Rick"
+last_name = "Morales"
+
+name = first_name + " " + last_name
+
+weight = 226
+
+sentence = first_name + " " + last_name + " weighs " + str(weight) + " lbs"
+
+print(sentence)
