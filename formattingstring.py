91df62379b7015b09c4270b0322bb1debc2cf44b
Rick Morales <rick.morales7693@gmail.com>
2023-11-10 17:44:04 -0600

    formatting strings

---
+++ formattingstring.py
@@ -0,0 +1,8 @@
+sentence = "The sum of 5 + 10 = {0}".format(5+10)
+print(sentence)
+
+sentence = "The sum of {0} + {1} = {2}".format(5, 10, 15)
+print(sentence)
+
+sentence = "The sum of {0} + {2} = {1}".format(5, 15, 10)
+print(sentence)
